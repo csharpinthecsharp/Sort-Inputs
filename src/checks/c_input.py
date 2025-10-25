5704bd034758ef338eafca9a116d11acaab824d1
vez <lucientrillard77120@gmail.com>
2025-10-26 00:51:49 +0200

    Enum def for errors

--- src/checks/c_input.py
+++ src/checks/c_input.py
@@ -1,10 +1,14 @@
+from data import d_enum
+
 def is_valid(av_len: int, av: list) -> bool:
     if av_len <= 1:
-            print("\nNo argument found")
+            print(d_enum.s_err.no_arg.value)
             return (False)
         
-    for char in av:
-        if (char.isalpha()):
-            print("\nYou must use numbers")
+    for i in av[1:]:
+        try:
+            int(i)
+        except ValueError:
+            print(d_enum.s_err.not_nbr.value)
             return (False)
     return (True)
---
+++ src/data/d_enum.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class s_err(Enum):
+     no_arg = "\nNo argument found"
+     not_nbr = "\nYou must use number"
